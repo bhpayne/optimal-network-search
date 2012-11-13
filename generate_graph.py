5b63f4b721ebcab9a1200a5524f44995b7bf0816
bpayne@lps.umd.edu <bpayne@lps.umd.edu@6408570b-47b8-b073-6cc2-3849ac582f3b>
2012-11-13 21:29:09 +0000

    random network configuration

--- generate_graph.py
+++ generate_graph.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python
 
 import os
+from random import choice
 
-def create_graphviz_file():
+def create_graphviz_file(computers,switches,connections):
   fil=open('network.gv', 'w')
 
   fil.write("##Command to produce the output: \"neato -Tpng thisfile.gv > thisfile.png\"\n")
   fil.write("graph G {\n")
-  fil.write("node [shape=box,color=red,style=bold];  c1; c2; c3; c4; c5; c6; c7;\n")
-  fil.write("node [shape=circle,fixedsize=true,width=0.9,color=blue,style=bold];  s1; s2; s3;\n")
-  fil.write("     c1 -- s1;\n")
-  fil.write("     c1 -- s2;\n")
-  fil.write("     c1 -- s3;\n")
-  fil.write("     c2 -- s1;\n")
-  fil.write("     c2 -- s2;\n")
-  fil.write("     c3 -- s1;\n")
-  fil.write("     c3 -- s3;\n")
-  fil.write("     c4 -- s3;\n")
-  fil.write("     c5 -- s3;\n")
-  fil.write("     c6 -- s1;\n")
-  fil.write("     c6 -- s2;\n")
-  fil.write("     c7 -- s2;\n")
-  fil.write("     c7 -- s3;\n")
+  for computer in computers:
+    fil.write("node [shape=box,color=red,style=bold];  c"+str(computer)+";\n")
+  for switch in switches:  
+    fil.write("node [shape=circle,fixedsize=true,width=0.9,color=blue,style=bold];  s"+str(switch)+";\n")
+
+  for switch_index,switch in enumerate(connections):
+    this_switch_is_connected_to_computers=switch
+    for computer in this_switch_is_connected_to_computers:
+      #print ("s"+str(switch_index)+"--c"+str(computer))
+      fil.write("     s"+str(switch_index)+"--c"+str(computer)+";\n")
   fil.write("     overlap=false\n")
   fil.write("     label=\"optimized network test\\nlayed out by Graphviz\"\n")
   fil.write("     fontsize=12;\n")
@@ -29,23 +25,37 @@ def create_graphviz_file():
   fil.close()
   return
 
-def generate_png_from_graphviz_file():
-  os.system("neato -Tpng network.gv > network.png")
-  return
-
-switch_list =[
-[1, 2, 3], # switch0 is connected to computers 1, 2, and 3
-[0, 2, 4]]    # switch1 is connected to computers 0, 2 and 4
 
 number_of_switches=2
 number_of_computers=5
 number_of_ports_per_computer=2
+number_of_ports_per_switch=4
 
-for switch_index,switch in enumerate(switch_list):
-  this_switch_is_connected_to_computers=switch
-  for computer in this_switch_is_connected_to_computers:
-    print ("s"+str(switch_index)+"--c"+str(computer))
+computers=range(number_of_computers)
+switches=range(number_of_switches)
 
-#create_graphviz_file
+#connections =[
+#[   1, 2, 3  ], # switch0 is connected to computers 1, 2, and 3
+#[0,    2,   4]]    # switch1 is connected to computers 0, 2 and 4
 
-#generate_png_from_graphviz_file
+connections=[] # declare new list
+
+# the following fills all switch ports with connections to computers
+# Problem: computer X can be connected to switch Y multiple times
+# Problem: computer X may not be connected to any switch
+for switch_index in switches:
+  print ("switch index="+str(switch_index))
+  port_list=[]
+  for port_index in range(number_of_ports_per_switch):
+    port_list.append(choice(computers))
+  connections.append(port_list)    
+
+# use all the ports on each computer to connect to switches
+
+print connections
+
+create_graphviz_file(computers,switches,connections)
+
+os.system("neato -Tpng network.gv > network.png")
+
+# EOF
