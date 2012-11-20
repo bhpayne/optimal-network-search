c1559ca2d5ad770530ea732d44732cb27b089d2a
bpayne@lps.umd.edu <bpayne@lps.umd.edu@6408570b-47b8-b073-6cc2-3849ac582f3b>
2012-11-20 15:41:27 +0000

    new scripts and pictures

---
+++ analyze_hops.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+# Ben Payne
+# bpayne@lps.umd.edu
+
+import itertools          # for generating pairs of computers 
+
+def find_distance(computerA,computerB,connections):
+  print (str(computerA)+"--"+str(computerB)+": ")
+  positions_for_computerA=find_positions_of_computer(connections,computerA)
+  positions_for_computerB=find_positions_of_computer(connections,computerB)
+  print ("locations of computer "+str(computerA)+": "+str(positions_for_computerA))
+  print ("locations of computer "+str(computerB)+": "+str(positions_for_computerB))
+  if ((len(positions_for_computerA)==1) and (len(positions_for_computerB)==1)): # both computers only have one connection
+    #print ("locations of computer "+str(computerA)+": "+str(positions_for_computerA))
+    #print ("locations of computer "+str(computerB)+": "+str(positions_for_computerB))
+    if (positions_for_computerA[0][0]==positions_for_computerB[0][0]): # the computers are on the same switch
+      print ("computer "+str(computerA)+" and "+str(computerB)+" are on the same switch")
+      hop_distance=1
+      return hop_distance
+    #else: # computers each have one connection and they are on different switches
+      ## is the switch computerA is connected to connected to the switch computerB is connected to?
+      #for other_switch in 
+      #if (positions_for_computerA[0][Y]==positions_for_computerB[0][X]):
+  elif (len(positions_for_computerA)==1): # computer A has only one port
+    for which_port_on_computerB in range(len(positions_for_computerB)):
+      if (positions_for_computerA[0][0]==positions_for_computerB[which_port_on_computerB][0]): # the computers are on the same switch
+	print ("computer "+str(computerA)+" and "+str(computerB)+" are on the same switch")
+	hop_distance=1
+	return hop_distance
+      #else: # computerA has one port and the two computers are on different switches
+  elif (len(positions_for_computerB)==1): # computer B has only one port
+    for which_port_on_computerA in range(len(positions_for_computerA)):
+      if (positions_for_computerB[0][0]==positions_for_computerA[which_port_on_computerA][0]): # the computers are on the same switch
+	print ("computer "+str(computerA)+" and "+str(computerB)+" are on the same switch")
+	hop_distance=1
+	return hop_distance
+      #else: # computerB has one port and the two computers are on different switches
+  else: # both switches have more than one port
+    for which_port_on_computerA in range(len(positions_for_computerA)):
+      for which_port_on_computerB in range(len(positions_for_computerB)):
+	if (positions_for_computerA[which_port_on_computerA][0]==positions_for_computerB[which_port_on_computerB][0]): # the computers are on the same switch
+	  print ("computer "+str(computerA)+" and "+str(computerB)+" are on the same switch")
+	  hop_distance=1
+	  return hop_distance
+        #else: # computers both have more than one port and the two computers are on different switches
+
+  
+def find_positions_of_computer(connections,computerA):
+  all_positions_for_indx_A = list(get_positions(connections,computerA))
+  # but we only care about computers with this index, not switches
+  # ignore positions of the form (X,1,X)
+  positions_for_computer=[]
+  for position_indx in range(len(all_positions_for_indx_A)):
+    if (all_positions_for_indx_A[position_indx][1]==0): # then this is a computer, not a switch
+      positions_for_computer.append(all_positions_for_indx_A[position_indx])
+  return positions_for_computer
+  
+# http://stackoverflow.com/questions/853023/how-can-i-find-the-locations-of-an-item-in-a-python-list-of-lists
+def get_positions(xs, item): # "xs" is the list of lists, "item" is what you are looking for
+    if isinstance(xs, list):
+        for i, it in enumerate(xs):
+            for pos in get_positions(it, item):
+                yield (i,) + pos
+    elif xs == item:
+        yield ()
+  
+connections=[
+[[0,1,2],[1]],
+[[0,3,4],[0]]]
+
+
+number_of_switches=len(connections)
+number_of_computers=5
+
+hop_array=[]
+pair_array=list(itertools.combinations(range(number_of_computers), 2))
+for pair_indx in range(len(pair_array)):
+  hop_distance=find_distance(pair_array[pair_indx][0],pair_array[pair_indx][1],connections)
+  hop_array.append(hop_distance)
+print hop_array
--- generate_graph.py
+++ generate_graph.py
@@ -2,39 +2,56 @@
 
 # Ben Payne
 # bpayne@lps.umd.edu
+# the purpose of this script is to build a random but valid network of computers and switches
+# --> avoid switch connecting to itself
+# --> avoid computer connect to itself
+# --> avoid redundant connections
+# --> avoid switches with no computers connected to them
+# --> avoid unconnected subset networks
+# --> if (number of switches>1) then each switch must connect to at least one other switch
+
 # see effbot.org/zone/python-list.htm
 
 import os
-from random import choice
-import itertools
+from random import choice # for "choice" in determining connections
+import itertools          # for generating pairs of computers 
 
-# http://stackoverflow.com/questions/5360220/how-to-split-a-list-into-pairs-in-all-possible-ways
-def all_pairs(lst):
-  if len(lst) < 2:
-    yield lst
-    return
-  a = lst[0]
-  for i in range(1,len(lst)):
-    pair = (a,lst[i])
-    for rest in all_pairs(lst[1:i]+lst[i+1:]):
-      yield [pair] + rest
-# By default Python has a return stack 1000 calls deep. You are recursing on pairs of digits, so this should not be an issue until your list is almost 2000 items long. At only 50 items you get more than 5*10^31 combinations; you will run into billion-year computations long before stack depth becomes an issue. 
+def sanity_checks(number_of_switches,number_of_computers,number_of_ports_per_computer,number_of_ports_per_switch):
+  # total number of ports on switches must be greater than number of compute nodes
+  if ((number_of_switches*number_of_ports_per_switch)<number_of_computers):
+    print ("total number of ports on switches must be greater than number of computers")
+    print ("number of computers="+str(number_of_computers))
+    print ("number of switches ="+str(number_of_switches))
+    print ("number of ports on switch="+str(number_of_ports_per_switch))
+    exit(1) # infinite loop would occur during search
+  if ((number_of_switches*number_of_ports_per_switch)==number_of_computers) and (number_of_switches>1):
+    print ("total number of ports on switches must be greater than number of computers")
+    print ("number of computers="+str(number_of_computers))
+    print ("number of switches ="+str(number_of_switches))
+    print ("number of ports on switch="+str(number_of_ports_per_switch))
+    exit(1) # infinite loop would occur during search
+  if (number_of_switches==1) and (number_of_ports_per_switch==number_of_computers):
+    print ("cross-bar network detected (number of ports per switch=number of computers, and number of switches=1")
 
-def create_graphviz_file(computers,switches,connections):
+  
+def create_graphviz_file(computers,connections):
   fil=open('network.gv', 'w')
 
   fil.write("##Command to produce the output: \"neato -Tpng thisfile.gv > thisfile.png\"\n")
   fil.write("graph G {\n")
   for computer in computers:
     fil.write("node [shape=box,color=red,style=bold];  c"+str(computer)+";\n")
-  for switch in switches:  
+  for switch in range(len(connections)):  
     fil.write("node [shape=circle,fixedsize=true,width=0.9,color=blue,style=bold];  s"+str(switch)+";\n")
 
-  for switch_index,switch in enumerate(connections):
-    this_switch_is_connected_to_computers=switch
+  for switch_index in range(len(connections)):
+    this_switch_is_connected_to_computers=connections[switch_index][0]
     for computer in this_switch_is_connected_to_computers:
       #print ("s"+str(switch_index)+"--c"+str(computer))
       fil.write("     s"+str(switch_index)+"--c"+str(computer)+";\n")
+    this_switch_is_connected_to_switches=connections[switch_index][1]
+    for other_switch in this_switch_is_connected_to_switches:
+      fil.write("     s"+str(switch_index)+"--s"+str(other_switch)+";\n")
   fil.write("     overlap=false\n")
   fil.write("     label=\"optimized network test\\nlayed out by Graphviz\"\n")
   fil.write("     fontsize=12;\n")
@@ -42,26 +59,43 @@ def create_graphviz_file(computers,switches,connections):
   fil.close()
   return
 
+def hops_between_nodes(computer_pairs,connections):
+  # option 1: exhaustive search
+    #1a: start by filling out the known distance when computer A and B are plugged into the same switch
+  #print connections
+  #print computer_pairs
+  for switch_index,switch_list in enumerate(connections):
+    print switch_list
+    pairs_on_this_switch=list(itertools.combinations(switch_list,2))
+    print pairs_on_this_switch
+  # option 2: take a random subset of computers and find the pair hopping distance
 
-number_of_switches=100
-number_of_computers=5
-number_of_ports_per_computer=2
-number_of_ports_per_switch=4
+number_of_switches=10
+number_of_computers=10
+number_of_ports_per_computer=3
+number_of_ports_per_switch=8
+
+sanity_checks(number_of_switches,number_of_computers,number_of_ports_per_computer,number_of_ports_per_switch)
 
 computers=range(number_of_computers)
 switches=range(number_of_switches)
 
-list(itertools.combinations(computers, 2))
-number_of_computer_pairs=len(list(itertools.combinations(computers, 2)))
+computer_pairs=[]
+pair_array=list(itertools.combinations(computers, 2))
+number_of_computer_pairs=len(pair_array)
+# convert from list to array:
+for pair_indx in range(number_of_computer_pairs):
+  pair_list=[]
+  pair_list.append(pair_array[pair_indx][0])
+  pair_list.append(pair_array[pair_indx][1])
+  computer_pairs.append(pair_list)
+# For n computers there are n*(n-1)/2 pairs
 #>>> len(list(itertools.combinations(range(100), 2)))
-# 4950
+#       4,950
 #>>> len(list(itertools.combinations(range(1000), 2)))
-# 499500
-# 
-
-#connections =[
-#[   1, 2, 3  ], # switch0 is connected to computers 1, 2, and 3
-#[0,    2,   4]]    # switch1 is connected to computers 0, 2 and 4
+#     499,500
+#>>> len(list(itertools.combinations(range(1000000), 2))) # this fails, but the answer would be
+# 499,999,500
 
 connections=[] # declare new list
 
@@ -80,24 +114,62 @@ connections=[] # declare new list
 # each computer should be connected to a switch
 # --> the constraints center on the computer, not the switches. Thus we need to iterate through the list of computers
 for indx in switches:
-  connections.append([]) # this creates a list of empty lists
+  connections.append([[],[]]) # this creates a list of empty lists [switch0, switch1,...] where switchX = [[computerK, computerB], [switchY]]
 for computer in computers:
   for port_indx in range(number_of_ports_per_computer):
-    which_switch_this_computer_connects_to=choice(switches)
-    if not computer in connections[which_switch_this_computer_connects_to]:     # if this computer is not already connected to this switch, then
-      connections[which_switch_this_computer_connects_to].append(computer)      # connect it
-#    else: # maybe a "while" would be better -- search for open switch
+    find_port=True
+    while find_port:
+      which_switch_this_computer_connects_to=choice(switches)
+      if (not computer in connections[which_switch_this_computer_connects_to][0]) and (len(connections[which_switch_this_computer_connects_to][0])+len(connections[which_switch_this_computer_connects_to][1])<number_of_ports_per_switch-1):     # if (this computer is not already connected to this switch) and (there are ), then
+        connections[which_switch_this_computer_connects_to][0].append(computer)      # connect it
+        find_port=False
+for switch in switches:
+  #for port_indx in range(number_of_ports_per_switch - len(connections[switch][0])): # fill in the remaining switch ports with connections to other switches
+    find_port=True
+    while find_port:
+      which_switch_this_switch_connects_to=choice(switches)
+      if (not switch in connections[which_switch_this_switch_connects_to][1]) and (not which_switch_this_switch_connects_to in connections[switch][1]): # and (which_switch_this_switch_connects_to != switch):     # if this switch is not already connected to this other switch), then
+        if (which_switch_this_switch_connects_to==switch): 
+          find_port=False
+          break
+        connections[which_switch_this_switch_connects_to][1].append(switch)      # connect it
+        find_port=False
+      if (len(connections[switch][0])+len(connections[switch][1]) == number_of_ports_per_switch): # this switch is full, no more connections should be made
+        print switch
+	find_port=False
 
 # if number of switches >> number of computers, then 
 #   1) not all switches are used [and thus don't need to be drawn]
 #   2) any given computer may not be able to connect to all other computers
-for switch in connections:
-  print len.connections[switch]
+#   3? Having more than one path between two computers. Possible, but useful?
 
-print connections
+# solution to problem 1, not all switches are used [and thus don't need to be drawn]
+newconnect=[]
+#newconnect2=[]
+newswitches=[]
+#newswitches2=[]
+for switch_indx in switches:
+  if len(connections[switch_indx][0])>1: # remove switches with 0 or 1 computer connections
+    newconnect.append(connections[switch_indx])
+    newswitches.append(switch_indx)
+    
+for swi in range(len(newconnect)):
+  print newconnect[swi]
+    
+create_graphviz_file(computers,newconnect)
 
-create_graphviz_file(computers,switches,connections)
+#hops_between_nodes(computer_pairs,newconnect)
 
-os.system("neato -Tpng network.gv > network.png")
+#neato - filter for drawing undirected graphs
+os.system("neato -Tpng network.gv > network_neato.png")
+#twopi - filter for radial layouts of graphs
+os.system("twopi -Tpng network.gv > network_twopi.png")
+#circo - filter for circular layout of graphs
+os.system("circo -Tpng network.gv > network_circo.png")
+#fdp - filter for drawing undirected graphs
+os.system("fdp -Tpng network.gv > network_fdp.png")
+#sfdp - filter for drawing large undirected graphs
+os.system("sfdp -Tpng network.gv > network_sfdp.png")
+
 
 # EOF
