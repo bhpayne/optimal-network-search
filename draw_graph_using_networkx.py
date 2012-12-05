e0165000547bf5abe271114b03ca619d880dbd8e
bpayne@lps.umd.edu <bpayne@lps.umd.edu@6408570b-47b8-b073-6cc2-3849ac582f3b>
2012-12-05 22:18:45 +0000

    all-to-all works!

---
+++ draw_graph_using_networkx.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import networkgraphio as ngio
+
+# http://networkx.lanl.gov/tutorial/tutorial.html
+
+number_of_switches,number_of_computers,connections=ngio.readGraphFromFile()
+
+G=nx.Graph()
+
+G.add_edges_from(connections)
+
+# The following colorization of nodes works but cannot be drawn
+#for nodeIndx, nodeValue in enumerate(G.nodes()):
+  #if (nodeValue<0):
+    #G[nodeValue]['color']='red'
+  #elif (nodeValue>0):
+    #G[nodeValue]['color']='blue'
+  #else:
+    #print ("error in connections node value")
+  
+
+nx.draw(G)
+plt.show()
+plt.savefig("networkx_draw.png")
+
+nx.draw_random(G)
+plt.show()
+plt.savefig("networkx_random.png")
+nx.draw_circular(G)
+plt.show()
+plt.savefig("networkx_circular.png")
+nx.draw_spectral(G)
+plt.show()
+plt.savefig("networkx_spectral.png")
--- fitness_function_all_to_all_hops.py
+++ fitness_function_all_to_all_hops.py
@@ -17,31 +17,40 @@ import itertools           # for generating pairs of computers
 # http://networkx.lanl.gov/reference/generated/networkx.algorithms.simple_paths.all_simple_paths.html#networkx.algorithms.simple_paths.all_simple_paths
 # http://networkx.lanl.gov/reference/algorithms.shortest_paths.html#module-networkx.algorithms.shortest_paths.unweighted
 
+def find_all_compute_hop_lengths(number_of_computers,G):
+  all_pairs=list(itertools.combinations(range(1,number_of_computers+1), 2))
+
+  # http://networkx.lanl.gov/reference/generated/networkx.algorithms.shortest_paths.generic.shortest_path_length.html#networkx.algorithms.shortest_paths.generic.shortest_path_length
+  all_lengths=[]
+  for pair_indx in range(len(all_pairs)):
+    computerA=all_pairs[pair_indx][0]*-1
+    computerB=all_pairs[pair_indx][1]*-1
+    length_between_compute_nodes=nx.shortest_path_length(G,source=computerA,target=computerB)
+    #print length_compute_nodes
+    all_lengths.append(length_between_compute_nodes)
+  return all_lengths
+    
 number_of_switches,number_of_computers,connections=ngio.readGraphFromFile()
 
 G=nx.Graph()
 
 G.add_edges_from(connections)
 
-# http://networkx.lanl.gov/reference/generated/networkx.algorithms.shortest_paths.dense.floyd_warshall.html#networkx.algorithms.shortest_paths.dense.floyd_warshall
-distnc=nx.floyd_warshall(G) # dictionary of all pair distances
-# now distnc.keys() == connections
-# the distance between any to nodes A, B is given by distnc[A][B]
-# as expected, distnc[A][A]==0
-# and an edge=1 unit of distance
+## http://networkx.lanl.gov/reference/generated/networkx.algorithms.shortest_paths.dense.floyd_warshall.html#networkx.algorithms.shortest_paths.dense.floyd_warshall
+#distnc=nx.floyd_warshall(G) # dictionary of all pair distances
+## now distnc.keys() == connections
+## the distance between any to nodes A, B is given by distnc[A][B]
+## as expected, distnc[A][A]==0
+## and an edge=1 unit of distance
 
 # alternative:
 # path=nx.all_pairs_shortest_path(G)
 
 #>>> len(list(itertools.combinations(range(1000), 2)))
 #     499,500
-
-all_pairs=list(itertools.combinations(range(1,number_of_computers+number_of_switches+1), 2))
-
-all_lengths=[]
-for pair_indx in range(len(all_pairs)):
-  print distnc[all_pairs[pair_indx][0]][all_pairs[pair_indx][1]]
-#   all_lengths.append(distnc[all_pairs[pair_indx][0]][all_pairs[pair_indx][1]])
+all_lengths=find_all_compute_hop_lengths(number_of_computers,G)
   
-# http://networkx.lanl.gov/reference/generated/networkx.algorithms.shortest_paths.generic.shortest_path_length.html#networkx.algorithms.shortest_paths.generic.shortest_path_length
-length_compute_nodes=nx.shortest_path_length(G,source=0,target=4))
+average_hop_count = float(sum(all_lengths)) / len(all_lengths)
+print ("average hop count is "+str(average_hop_count))
+max_hop_count = max(all_lengths)
+print ("maximum hop count is "+str(max_hop_count))
