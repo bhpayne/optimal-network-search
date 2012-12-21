13a4b43332acf4c1133d1ffb2dd1bc40d4031a31
bpayne@lps.umd.edu <bpayne@lps.umd.edu@6408570b-47b8-b073-6cc2-3849ac582f3b>
2012-12-21 14:45:44 +0000

    works

--- bisection_examples/bisection_example_randomly_selected_routers_only_2partitions.py
+++ bisection_examples/bisection_example_randomly_selected_routers_only_2partitions.py
@@ -6,16 +6,32 @@ number_of_routers=20
 G=nx.cycle_graph(number_of_routers) # whatever graph you want to use here
 
 all_routers=range(1,number_of_routers+1) 
+print all_routers
 random.shuffle(all_routers)
 # now split this list into two parts of unequal size
-random_number_of_routers=random.randint(0,number_of_routers)
+random_number_of_routers= random.randint(0,number_of_routers)
 
-left_partition_routers=all_routers[0:random_number_of_routers]
-right_partition_routers=all_routers[random_number_of_routers+1:number_of_routers]
+print ("random number of routers="+str(random_number_of_routers))
+left_partition=all_routers[0:random_number_of_routers]
+right_partition=all_routers[random_number_of_routers:number_of_routers+1]
+
+print left_partition
+print right_partition
+
+connections=G.edges()
+bisection_count=0
+print G.edges()
+print "bisecting edges:"
+for edge in connections:
+  if (((edge[0]+1 in left_partition) and (edge[1]+1 in right_partition)) or ((edge[0]+1 in right_partition) and (edge[1]+1 in left_partition))):
+    print edge
+    bisection_count=bisection_count+1
+
+print ("bisection count="+str(bisection_count))
 
 node_color_list=[]
 for router_indx in range(1,number_of_routers+1):
-  if router_indx in left_partition_routers:
+  if router_indx in left_partition:
     node_color_list.append('blue')
   else:
     node_color_list.append('red')
