c18486dcbfafc66884de4192b77458af5d63f4a0
bpayne@lps.umd.edu <bpayne@lps.umd.edu@6408570b-47b8-b073-6cc2-3849ac582f3b>
2012-12-27 18:52:58 +0000

    separated drawers. Nothing tested yet

--- search_drawers.py
+++ search_drawers.py
@@ -27,10 +27,8 @@
 
 import networkx as nx
 import lib_network_optimization as nopt # Ben's module for graph input/output
-import itertools           # for generating pairs of computers 
 import matplotlib.pyplot as plt
 import random
-from operator import mul # compute a product
 import math
 import yaml
 import time
@@ -50,16 +48,16 @@ def done_searching(metric_initial,metric_best,connections_best,tracker,metric_na
   plt.close()
   print("Total elapsed time: "+str(time.clock()-t_start)+" seconds")
 
-def how_many_picks(confidence,number_of_computers,number_of_routers,max_picks):
+def how_many_picks(confidence,number_of_drawers,max_picks):
   #number of picks p = \frac{\log(1-c)}{\log(1-(1/U))}
   #where U = M! \prod_{x=0}^{N/2}(N-x)
-  if ((number_of_computers%2)==0):
-    total_number = math.factorial(number_of_routers)*reduce(mul,range(number_of_computers/2,number_of_computers))
+  if ((number_of_drawers%2)==0):
+    total_number = math.pow(2,(number_of_drawers/2))
   else:
-    total_number = math.factorial(number_of_routers)*reduce(mul,range((number_of_computers+1)/2,number_of_computers))
+    total_number = math.pow(2,((number_of_drawers+1)/2))
   if (math.log(1.0-(1.0/(total_number+0.0)))==0):
-    print("warning: total number of permutations "+str(total_number)+" is too large for python float.")
-    print("Setting number of picks = 100")
+    print("warning: total number of permutations "+str(total_number)+" is too large for search (limited by float).")
+    print("Setting number of picks = "+str(max_picks))
     # total_number is almost always too large for realistic networks to find bisection minimum with any confidence level
     number_of_picks=max_picks
   else:
@@ -84,14 +82,10 @@ connections=input_data["connections"]
 use_hop_count=input_data["use_hop_count"]
 use_simulated_annealing=input_data["use_simulated_annealing"]
 
-number_of_computers=input_data["number_of_computers"]
-number_of_ports_per_computer=input_data["number_of_ports_per_computer"]
-number_of_routers=input_data["number_of_routers"]
-number_of_ports_per_router=input_data["number_of_ports_per_router"]
+number_of_drawers=input_data["number_of_drawers"]
+number_of_ports_per_drawer=input_data["number_of_ports_per_drawer"]
 
 input_stream.close()
-
-nopt.sanity_checks(number_of_routers,number_of_computers,number_of_ports_per_computer,number_of_ports_per_router)
 
 t_read_input = time.clock() - t_start # CPU seconds elapsed 
 #print("time to read input: "+str(t_read_input)+" seconds")
@@ -101,17 +95,10 @@ search_indx=0
 while ((not found_valid_initial_graph) and (search_indx<random_network_search_limit)):
 
   if (not specify_connections_input):
-    connections = nopt.generate_random_network(number_of_computers,number_of_ports_per_computer,number_of_routers,number_of_ports_per_router,random_network_search_limit)
+    connections = nopt.generate_random_drawer_network(number_of_drawers,number_of_ports_per_drawer,random_network_search_limit)
   
-  #alternatives to random graph:
-  #connections = nopt.generate_2D_mesh_network(number_of_rows,number_of_columns)
-  #and
-  #connections = nopt.generate_ND_toroidal_or_mesh_network(dimensions,toroidal_true_mesh_false)
-  #as an example,
-  #dimensions=[2,3,5]
-  #toroidal_true_mesh_false=True
   try:
-    hop_count_distribution=nopt.fitness_function_find_all_compute_hop_lengths(number_of_computers,connections)
+    hop_count_distribution=nopt.fitness_function_find_all_drawer_hop_lengths(number_of_drawers,connections)
     found_valid_initial_graph=True
     break
   except nx.NetworkXNoPath:
@@ -126,7 +113,7 @@ stream=file('network_connections_initial.log','w')
 yaml.dump({'connections':connections},stream)
 stream.close()
 
-nopt.draw_graph_pictures(connections_best,"initial")
+nopt.draw_drawer_graph_pictures(connections_best,"initial")
 
 tracker=[]
 if (use_hop_count):
@@ -139,10 +126,10 @@ if (use_hop_count):
 else:
   metric_name_file="minimum_bisection_count"
   metric_name_label="minimum bisection count"
-  number_of_picks=how_many_picks(confidence_of_finding_minimum_bisection,number_of_computers,number_of_routers,max_picks)
+  number_of_picks=how_many_picks(confidence_of_finding_minimum_bisection,number_of_drawers,max_picks)
   bisection_array=[]
   for bcount in range(number_of_picks):
-    bisection_count=nopt.fitness_function_bisection_count(number_of_computers,number_of_routers,connections)
+    bisection_count=nopt.fitness_function_bisection_count_drawers(number_of_drawers,connections)
     bisection_array.append(bisection_count)
   metric_best=min(bisection_array)
   metric_initial=min(bisection_array)
@@ -154,10 +141,10 @@ while (temperature_indx<number_of_iterations):
   search_indx=0
   while ((not found_valid_mutation) and (search_indx<valid_path_search_limit)):
     for alteration_indx in range(random.randint(1,max_number_of_swaps)):
-      connections_new=nopt.make_alteration_swap_ports(number_of_routers,number_of_computers,connections,random_network_search_limit)
-    connections_new=nopt.make_alteration_add_router_router_edge(number_of_routers,connections_new,number_of_ports_per_router,random_network_search_limit)
+      connections_new=nopt.make_alteration_swap_ports_drawers(number_of_drawers,connections,random_network_search_limit)
+    connections_new=nopt.make_alteration_add_router_router_edge(number_of_drawers,connections_new,number_of_ports_per_drawer,random_network_search_limit)
     try:
-      hop_count_distribution_new=nopt.fitness_function_find_all_compute_hop_lengths(number_of_computers,connections_new)
+      hop_count_distribution_new=nopt.fitness_function_find_all_drawer_hop_lengths(number_of_drawers,connections_new)
       found_valid_mutation=True
       break
     except nx.NetworkXNoPath:
@@ -180,7 +167,7 @@ while (temperature_indx<number_of_iterations):
   else: # bisection
     bisection_array=[]
     for bcount in range(number_of_picks):
-      bisection_count=nopt.fitness_function_bisection_count(number_of_computers,number_of_routers,connections)
+      bisection_count=nopt.fitness_function_bisection_count_drawers(number_of_drawers,connections)
       bisection_array.append(bisection_count)
     metric_new=min(bisection_array)
   if (metric_new<metric_best):
